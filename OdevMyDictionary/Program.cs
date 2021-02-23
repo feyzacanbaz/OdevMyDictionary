386c57c6a5a03436a5a5b7b523b880bb2dd2a563
feyza canbaz <feyza_canbaz@hotmail.com>
2021-02-23 13:55:56 +0300

    Add project files.

---
+++ OdevMyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OdevMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+
+            myDictionary.Add("Suc ve Ceza");
+            myDictionary.Add("Bülbülü Öldürmek");
+            myDictionary.Add("Mavi ve Siyah");
+
+
+        }
+    }
+}
+
